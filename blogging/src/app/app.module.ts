08bbd02f2ebac27046419cd68fd47e48e904dff9
Ramesha-GP~ <Ramesha.P@omnicell.com>
2023-08-21 18:06:32 +0530

    blog form

--- blogging/src/app/app.module.ts
+++ blogging/src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { WriteComponent } from './write/write.component';
+import { BlogFormComponent } from './blog-form/blog-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    WriteComponent
+    BlogFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
