d14bb43bef7ef5bee6a525835d91f4357cc789a9
Aanchal Shah <aanchalshah20182gmail.com>
2023-09-13 22:40:00 +0530

    Update Edit Blog Validations

--- blogging/src/app/edit-blog/edit-blog.component.ts
+++ blogging/src/app/edit-blog/edit-blog.component.ts
@@ -1,3 +1,37 @@
+// import { Component, OnInit } from '@angular/core';
+// import { ActivatedRoute, Router } from '@angular/router';
+// import { BlogService } from '../blog.service';
+
+// @Component({
+//   selector: 'app-edit-blog',
+//   templateUrl: './edit-blog.component.html',
+//   styleUrls: ['./edit-blog.component.scss']
+// })
+// export class EditBlogComponent implements OnInit {
+//   blogIndex!: number;
+//   blogData: any;
+
+//   constructor(
+//     private route: ActivatedRoute,
+//     private router: Router,
+//     private blogService: BlogService
+//   ) {}
+
+//   ngOnInit(): void {
+//     this.route.params.subscribe((params) => {
+//       this.blogIndex = +params['index'];
+//     });
+//     this.blogService.getAllPosts().subscribe(data => {
+//       this.blogData = data[this.blogIndex];
+//     });
+//   }
+
+//   updateBlog(): void {
+//     this.blogService.updateBlog(this.blogIndex, this.blogData);
+//     this.router.navigate(['/profile']);
+//   }
+// }
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BlogService } from '../blog.service';
@@ -10,6 +44,7 @@ import { BlogService } from '../blog.service';
 export class EditBlogComponent implements OnInit {
   blogIndex!: number;
   blogData: any;
+  descriptionValid: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -23,12 +58,18 @@ export class EditBlogComponent implements OnInit {
     });
     this.blogService.getAllPosts().subscribe(data => {
       this.blogData = data[this.blogIndex];
+      this.checkDescriptionLength();
     });
   }
 
   updateBlog(): void {
-    this.blogService.updateBlog(this.blogIndex, this.blogData);
-    this.router.navigate(['/profile']);
+    if (this.descriptionValid) {
+      this.blogService.updateBlog(this.blogIndex, this.blogData);
+      this.router.navigate(['/profile']);
+    }
+  }
+
+  checkDescriptionLength(): void {
+    this.descriptionValid = this.blogData.description && this.blogData.description.length >= 25;
   }
 }
-
