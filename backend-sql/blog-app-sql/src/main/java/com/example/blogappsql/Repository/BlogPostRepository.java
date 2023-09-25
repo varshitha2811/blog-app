da28a6ddd64a20b8e41f8bcaf527e79d2b1fb98b
Aanchal Shah <71883236+aanchalsh@users.noreply.github.com>
2023-09-25 16:54:44 +0530

    Delete backend-sql directory

--- backend-sql/blog-app-sql/src/main/java/com/example/blogappsql/Repository/BlogPostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.blogappsql.Repository;
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.blogappsql.Entity.Blog;
-
-public interface BlogPostRepository extends CrudRepository<Blog, Long> {
-}
