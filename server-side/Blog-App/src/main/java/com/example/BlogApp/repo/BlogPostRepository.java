9d8dd1eaa94d120abf57fafe1cef5a8311e236bd
Aanchal Shah <71883236+aanchalsh@users.noreply.github.com>
2023-09-25 16:55:01 +0530

    Delete server-side/Blog-App directory

--- server-side/Blog-App/src/main/java/com/example/BlogApp/repo/BlogPostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.BlogApp.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.BlogApp.Entity.Blog;
-public interface BlogPostRepository extends MongoRepository<Blog,String> {
-
-}
