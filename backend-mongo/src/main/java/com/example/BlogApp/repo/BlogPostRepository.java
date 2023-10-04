583b03c43b9b468ef4fa9c82bae3bbe37546f71c
Aanchal Shah <71883236+aanchalsh@users.noreply.github.com>
2023-10-04 15:01:34 +0530

    Update BlogPostRepository.java

--- backend-mongo/src/main/java/com/example/BlogApp/repo/BlogPostRepository.java
+++ backend-mongo/src/main/java/com/example/BlogApp/repo/BlogPostRepository.java
@@ -1,10 +1,16 @@
 package com.example.BlogApp.repo;
 
-
+import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.BlogApp.Entity.Blog;
 
 public interface BlogPostRepository extends MongoRepository<Blog, String> {
+
+	List<Blog> findByAuthor(String username);
+
+	List<Blog> findByUserName(String username);
+
+	List<Blog> findByuserName(String username);
 }
