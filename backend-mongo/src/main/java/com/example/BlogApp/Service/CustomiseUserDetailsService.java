317b783ed9d701df6ea7746b5e37703a797671fd
Aanchal Shah <71883236+aanchalsh@users.noreply.github.com>
2023-10-04 15:04:28 +0530

    Update CustomiseUserDetailsService.java

--- backend-mongo/src/main/java/com/example/BlogApp/Service/CustomiseUserDetailsService.java
+++ backend-mongo/src/main/java/com/example/BlogApp/Service/CustomiseUserDetailsService.java
@@ -1,26 +1,26 @@
-//package com.example.BlogApp.Service;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//
-//import com.example.BlogApp.Entity.User;
-//import com.example.BlogApp.repo.UserReposiotory;
-//
-//public class CustomiseUserDetailsService implements UserDetailsService{
-//	@Autowired
-//	private UserReposiotory usersRepository;
-//
-//	@Override
-//	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//		User user = usersRepository.findByUserName(username);
-//		if (user == null) {
-//			throw new UsernameNotFoundException("User not found with UserName: " + username);
-//		}
-//		return user;
-//	}
-//}
+// package com.example.BlogApp.Service;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+// import com.example.BlogApp.Entity.User;
+// import com.example.BlogApp.repo.UserReposiotory;
+
+// public class CustomiseUserDetailsService implements UserDetailsService{
+// 	@Autowired
+// 	private UserReposiotory usersRepository;
+
+// 	@Override
+// 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+// 		User user = usersRepository.findByUserName(username);
+// 		if (user == null) {
+// 			throw new UsernameNotFoundException("User not found with UserName: " + username);
+// 		}
+// 		return user;
+// 	}
+// }
 
 package com.example.BlogApp.Service;
 
@@ -33,20 +33,22 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.example.BlogApp.Entity.User;
 import com.example.BlogApp.repo.UserReposiotory;
-
+ 
 @Service
 public class CustomiseUserDetailsService implements UserDetailsService {
 
-	@Autowired
-	private UserReposiotory userRepository;
+    @Autowired
+    private UserReposiotory userRepository;
 
-	@Override
-	@Transactional
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User user = userRepository.findByUserName(username);
-		if (user == null) {
-			throw new UsernameNotFoundException("User not found with username: " + username);
-		}
-		return user;
-	}
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+
+        return new CustomUserDetails(user); 
+    }
 }
