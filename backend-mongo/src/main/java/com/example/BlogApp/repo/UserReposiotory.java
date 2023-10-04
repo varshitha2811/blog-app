1a58737fc11e68828fc72514b267c0bdc9802232
Aanchal Shah <71883236+aanchalsh@users.noreply.github.com>
2023-10-04 15:01:08 +0530

    Update UserReposiotory.java

--- backend-mongo/src/main/java/com/example/BlogApp/repo/UserReposiotory.java
+++ backend-mongo/src/main/java/com/example/BlogApp/repo/UserReposiotory.java
@@ -1,8 +1,11 @@
 package com.example.BlogApp.repo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import com.example.BlogApp.Entity.User;
 
 public interface UserReposiotory extends MongoRepository<User, String>{
-	public User findByUserName(String UserName);
+	public User findByUserName(String userName);
+	boolean existsByUsername(String username);
+
 }
