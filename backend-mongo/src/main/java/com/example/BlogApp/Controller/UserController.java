a3905ffe3e54fe97f0b9eb580282233f0d7f0008
Aanchal Shah <71883236+aanchalsh@users.noreply.github.com>
2023-10-04 14:57:44 +0530

    Update UserController.java

--- backend-mongo/src/main/java/com/example/BlogApp/Controller/UserController.java
+++ backend-mongo/src/main/java/com/example/BlogApp/Controller/UserController.java
@@ -1,7 +1,5 @@
 package com.example.BlogApp.Controller;
 
-
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -11,26 +9,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.BlogApp.Entity.User;
+import com.example.BlogApp.Service.CustomUserDetails;
 
 @CrossOrigin(origins = "http://localhost:5000")
-@RestController
+ @RestController
 public class UserController {
-	@GetMapping("/current-user")
-	public ResponseEntity<User> getCurrentUser() {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		System.out.println("Authentication");
-		if (authentication == null || !authentication.isAuthenticated()) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-		}
-		if (authentication.getPrincipal() instanceof User) {
-			User userDetails = (User) authentication.getPrincipal();
-			User userInfo = new User();
-			userInfo.setName(userDetails.getName());
-			userInfo.setUserName(userDetails.getUserName());
-			return ResponseEntity.ok(userInfo);
-		} else {
-			System.out.println("Principal is not UserDetails: " + authentication.getPrincipal());
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-		}
-	}    
+
+    @GetMapping("/current-user")
+    public ResponseEntity<User> getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("Authentication");
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+			
+        }
+        if (authentication.getPrincipal() instanceof CustomUserDetails) {
+            CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+            User userInfo = userDetails.getUser();  // Access the User object directly
+            System.out.println("Principal is  CustomUserDetails: " + authentication.getPrincipal());
+            return ResponseEntity.ok(userInfo);
+        } else {
+            System.out.println("Principal is not CustomUserDetails: " + authentication.getPrincipal());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
 }
