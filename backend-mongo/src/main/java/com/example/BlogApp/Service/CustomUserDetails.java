b39935f5595c56a023b60f4d9ac74b6ca2a3c8a8
Aanchal Shah <71883236+aanchalsh@users.noreply.github.com>
2023-10-04 15:05:02 +0530

    Create CustomUserDetails.java

---
+++ backend-mongo/src/main/java/com/example/BlogApp/Service/CustomUserDetails.java
@@ -0,0 +1,61 @@
+package com.example.BlogApp.Service;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.BlogApp.Entity.User;
+
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+    }
+    public User getUser() {
+        return user;
+    }
+    @Override
+    public String getPassword() {
+        return user.getPassword(); 
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+   
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    
+    public boolean userDisabled() {
+        return false;
+    }
+}
